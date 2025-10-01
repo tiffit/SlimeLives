5e842f76584832136f3bfa511f0d67eee31eb0a7
tiffit <10173431+tiffit@users.noreply.github.com>
2025-10-01 10:57:31 -0700

    Basic platforming

---
+++ character/character.gd
@@ -0,0 +1,20 @@
+class_name Character extends CharacterBody2D
+
+@export var speed: float = 400.0
+@export var jump_speed: float = 400.0
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta: float) -> void:
+	# Gravity
+	velocity.y += gravity * delta
+	
+	# Horizontal movement
+	var horizontal: float = Input.get_axis("move_left", "move_right")
+	velocity.x = horizontal * speed
+	
+	# Jump
+	if is_on_floor() and Input.is_action_just_pressed("move_jump"):
+		velocity.y = -jump_speed
+	
+	# Do movement physics
+	move_and_slide()
