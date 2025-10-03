eb0b56dfc1a7345553703f2f69cc7730f6151546
tiffit <10173431+tiffit@users.noreply.github.com>
2025-10-02 18:46:17 -0700

    typo

--- ui/vignette/vignette.gd
+++ ui/vignette/vignette.gd
@@ -3,13 +3,13 @@ class_name Vignette extends ColorRect
 var shader_material = material as ShaderMaterial
 
 func _ready() -> void:
-	change_progresss(0.0)
+	change_progress(0.0)
 
 func flash_vignette():
 	var tween = create_tween()
-	tween.tween_method(change_progresss, 0.0, 1.0, 0.2).set_trans(Tween.TRANS_LINEAR)
-	tween.tween_method(change_progresss, 1.0, 0.0, 0.5).set_trans(Tween.TRANS_LINEAR)
+	tween.tween_method(change_progress, 0.0, 1.0, 0.2).set_trans(Tween.TRANS_LINEAR)
+	tween.tween_method(change_progress, 1.0, 0.0, 0.5).set_trans(Tween.TRANS_LINEAR)
 	tween.play()
 		
-func change_progresss(value: float):
+func change_progress(value: float):
 	shader_material.set_shader_parameter("progress", value)
