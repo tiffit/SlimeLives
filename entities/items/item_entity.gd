d2536cbaf16f7ee0ca75c049403aa3d391065d52
tiffit <10173431+tiffit@users.noreply.github.com>
2025-10-03 20:39:32 -0700

    Items can now be picked up instantly after cooldown

--- entities/items/item_entity.gd
+++ entities/items/item_entity.gd
@@ -20,6 +20,7 @@ func update_render():
 		
 func spit():
 	pickup_cooldown = 0.2
+	$Area2D.monitoring = false
 	var mouse_pos: Vector2 = get_viewport().get_mouse_position() / get_viewport_rect().size
 	var angle: float = Vector2(.5, .5).angle_to_point(mouse_pos)
 	var launch_vector: Vector2 = Vector2(1.0, 0).rotated(angle)
@@ -27,11 +28,12 @@ func spit():
 		
 func _process(delta: float) -> void:
 	pickup_cooldown = max(0, pickup_cooldown - delta)
+	if pickup_cooldown == 0:
+		$Area2D.monitoring = true
 
 func _on_player_entered(body: Node2D) -> void:
-	if pickup_cooldown == 0:
-		if body is Character and not is_queued_for_deletion():
-			if body.item == null:
-				if item:
-					body.pickup_item(item)
-				queue_free()
+	if body is Character and not is_queued_for_deletion():
+		if body.item == null:
+			if item:
+				body.pickup_item(item)
+			queue_free()
