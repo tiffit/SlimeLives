6c75b866855a575308234f6dbe027cc1dd24207e
Marshmalla <marshmallarotmg@gmail.com>
2025-10-05 13:38:01 -0700

    door opacity, map adjustments, more tutorial implementation, witch's tower stuff

--- entities/activatable/door.gd
+++ entities/activatable/door.gd
@@ -1,4 +1,5 @@
 class_name Door extends StaticBody2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 @export var activation_id: int = 0
 var activated: bool = false
@@ -7,8 +8,11 @@ func activate(id: int):
 	if id == activation_id and not activated:
 		activated = true
 		process_mode = Node.PROCESS_MODE_DISABLED
+		modulate = Color(1.0, 1.0, 1.0, 0.196)
+
 
 func deactivate(id: int):
 	if id == activation_id and activated:
 		activated = false
 		process_mode = Node.PROCESS_MODE_INHERIT
+		modulate = Color(1.0, 1.0, 1.0, 1.0)
