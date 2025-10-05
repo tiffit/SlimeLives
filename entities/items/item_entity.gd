33b0d38c2b14d8e54292deff8cdcc05275b380d4
tiffit <10173431+tiffit@users.noreply.github.com>
2025-10-04 19:55:59 -0700

    bomb

--- entities/items/item_entity.gd
+++ entities/items/item_entity.gd
@@ -8,6 +8,8 @@
 		update_render()
 
 var pickup_cooldown: float = 0
+var was_spit: bool = false
+const explosion_radius = 4
 
 func _ready() -> void:
 	update_render()
@@ -19,8 +21,11 @@ func update_render():
 		$ItemSprite.texture = null
 		
 func spit(character: Character):
+	was_spit = true
+	if item.explode:
+		$Timer.start()
 	pickup_cooldown = 0.2
-	$Area2D.monitoring = false
+	$PickupArea.monitoring = false
 	var mouse_pos: Vector2 = get_viewport().get_mouse_position() / get_viewport_rect().size
 	var player_pos: Vector2 = character.get_global_transform_with_canvas().get_origin() / get_viewport_rect().size
 	var angle: float = player_pos.angle_to_point(mouse_pos)
@@ -28,13 +33,32 @@ func spit(character: Character):
 	call_deferred("apply_central_impulse", launch_vector * 1000)
 		
 func _process(delta: float) -> void:
-	pickup_cooldown = max(0, pickup_cooldown - delta)
-	if pickup_cooldown == 0:
-		$Area2D.monitoring = true
+	if !Engine.is_editor_hint():
+		pickup_cooldown = max(0, pickup_cooldown - delta)
+		if pickup_cooldown == 0:
+			$PickupArea.monitoring = true
 
 func _on_player_entered(body: Node2D) -> void:
+	if item and item.explode and was_spit:
+		return
 	if body is Character and not is_queued_for_deletion():
 		if body.item == null:
 			if item:
 				body.pickup_item(item)
 			queue_free()
+
+func _on_timer_timeout() -> void:
+	if item.explode:
+		var item_tile_pos: Vector2 = position / 5 / 16
+		for child in get_parent().get_children():
+			if child is TileMapLayer:
+				for x in range(-explosion_radius, explosion_radius):
+					for y in range(-explosion_radius, explosion_radius):
+						var tile_pos: Vector2 = item_tile_pos + Vector2(x, y)
+						var int_tile_pos: Vector2i = Vector2i(tile_pos)
+						var tile_data: TileData = child.get_cell_tile_data(int_tile_pos)
+						if tile_data:
+							if tile_data.has_custom_data("fragile") and tile_data.get_custom_data("fragile"):
+								child.erase_cell(int_tile_pos)
+		queue_free()
+	
--- item/item.gd
+++ item/item.gd
@@ -3,3 +3,4 @@ class_name Item extends Resource
 @export var item_name: String
 @export var texture: Texture2D
 @export var platform: PackedScene
+@export var explode: bool = false
