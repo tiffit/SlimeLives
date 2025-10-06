f6ca2d67b7fb11bd83e0f541bd369393459505b7
tiffit <10173431+tiffit@users.noreply.github.com>
2025-10-05 17:43:00 -0700

    added esc to go back to main menu from level select

--- ui/main_menu/level_select/level_select.gd
+++ ui/main_menu/level_select/level_select.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var level_info: LevelInfo
 @export var game_main_scene: PackedScene
 @export var blank_collectible: Texture2D
+@export var click_sound: AudioStream
 
 func _ready() -> void:
 	%Background.texture = Globals.menu_background
@@ -11,7 +12,7 @@ func _ready() -> void:
 	var completed: Array[String] = SaveHelper.data.completed
 	var collected: Array[String] = SaveHelper.data.collected
 	
-	var previous_completed: bool = false
+	var previous_completed: bool = true
 	for level_scene in level_info.levels:
 		var level: Level = level_scene.instantiate()
 		var btn: Button = level_select_button_scene.instantiate()
@@ -36,6 +37,11 @@ func _ready() -> void:
 			btn.get_node("Collectible").texture = null
 		level.queue_free()
 		
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("toggle_options"):
+		get_tree().change_scene_to_file("res://ui/main_menu/main_menu.tscn")
+		
 func load_level(index: int):
 	Globals.next_level_index = index
+	MusicController.play_one_shot(click_sound)
 	get_tree().change_scene_to_packed(game_main_scene)
