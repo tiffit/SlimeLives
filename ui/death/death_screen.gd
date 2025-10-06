24ed29fea486b4bf5b9f38efbb7c39f7e885f01c
tiffit <10173431+tiffit@users.noreply.github.com>
2025-10-05 17:02:25 -0700

    Finished death screen

--- ui/death/death_screen.gd
+++ ui/death/death_screen.gd
@@ -2,6 +2,8 @@ class_name DeathScreen extends Control
 
 func _ready() -> void:
 	MusicController.go_to_pitch(0.5, 3)
+	$AnimationPlayer.play("turn")
+	$FadeInPlayer.play("fade_in")
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("reset_level") and !is_queued_for_deletion():
