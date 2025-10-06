8e101fbf1f83743f336571ceb145058560027a12
tiffit <10173431+tiffit@users.noreply.github.com>
2025-10-05 18:32:10 -0700

    main menu cat

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,20 +1,25 @@
-extends Node2D
+extends Control
 
 @export var options_menu_scene: PackedScene
 @export var game_main_scene: PackedScene
 @export var level_select_scene: PackedScene
 @export var level_info: LevelInfo
 @export var click_sound: AudioStream
+@export var meow_sound: AudioStream
 
 var next_level_index: int = 0
 var next_level_region: Level.LevelRegion = Level.LevelRegion.TWISTED_FOREST
 var seen_intro_comic: bool
+var slime_face_pos: Vector2
 
 func _ready() -> void:
-	if(SaveHelper.is_ready):
+	if SaveHelper.is_ready:
 		load_save_data()
 	else:
 		SaveHelper.on_ready.connect(load_save_data)
+	
+	slime_face_pos = %SlimeFace.global_position + %SlimeFace.size/2
+	print(slime_face_pos)
 
 func load_save_data():
 	var data: SaveData = SaveHelper.data
@@ -58,3 +63,14 @@ func _on_reset_save_data_btn_pressed() -> void:
 	Globals.next_level_index = -1
 	MusicController.play_one_shot(click_sound)
 	get_tree().change_scene_to_file("res://ui/main_menu/main_menu.tscn")
+
+func _on_slime_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			MusicController.play_one_shot(meow_sound)
+
+func _process(delta: float) -> void:
+	var offset: Vector2 = get_viewport().get_mouse_position() - slime_face_pos
+	if offset.length() > 15:
+		offset = offset/offset.length() * 15
+	%SlimeFace.global_position = slime_face_pos + offset - %SlimeFace.size/2
